79e04477d7b70f7feab7e18ce821e7e94f03703d
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-03-06 12:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 //your JS code here. If required.
-async function displayMessage(text, delay) {
-    await new Promise(resolve => setTimeout(resolve, delay));
-    document.getElementById('output').innerText = text;
-}
+// async function displayMessage(text, delay) {
+//     await new Promise(resolve => setTimeout(resolve, delay));
+//     document.getElementById('output').innerText = text;
+// }
 
-// Add event listener to the button
-document.getElementById('btn').addEventListener('click', async function() {
-    let text = document.getElementById('text').value;
-    let delay = document.getElementById('delay').value;
-    await displayMessage(text, delay);
-});
+// // Add event listener to the button
+// document.getElementById('btn').addEventListener('click', async function() {
+//     let text = document.getElementById('text').value;
+//     let delay = document.getElementById('delay').value;
+//     await displayMessage(text, delay);
+// });
+
+let output = document.getElementById("output");
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+
+document.getElementById("btn"),addEventListener("click", async ()=>{
+	let p = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve();
+		},delay.value)
+	})
+	let res = await p;
+	output.innerText = text.value;
+})
